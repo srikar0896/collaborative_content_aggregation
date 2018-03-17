6c21588173d13ed3954330221e380b34bdc943a3
srikar.ch <srikar.0896@gmail.com>
2018-03-17 11:41:10 +0530

    Update app.js

--- app.js
+++ app.js
@@ -6,9 +6,7 @@ app.controller('mainController',['$scope','$state','userInfoService',function($s
 }]);
 
 angular.module("app").run(function ($rootScope, $state, userInfoService) {
-  console.log("sawf");
   $rootScope.$on("$stateChangeStart", function(event, toState, toParams, fromState, fromParams){
-    console.log("commoom");
     if (toState.authenticate && !userInfoService.getUserInfo()["isAuthenticated"]){
       // User isn’t authenticated
       $state.go("login");
